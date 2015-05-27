80bcf97478d177ee31cdb3011997460899aae424
kellhellia <kellhellia@gmail.com>
2015-05-27 14:57:34 +0300

    add html:

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  $('.your-class').slick();
+  $('.slick').slick({
+  	autoplay: true,
+  	autoplaySpeed: 2000,
+  	pauseOnHover: false
+  });
+  $('.slick-case').slick({
+  	dots: true,
+  	arrows:false,
+  	autoplay: true,
+  	autoplaySpeed: 3000,
+  	pauseOnHover: false
+  });
+
+  $('#myModal').on('shown.bs.modal', function () {
+	  $('#myInput').focus()
+	})
 });
